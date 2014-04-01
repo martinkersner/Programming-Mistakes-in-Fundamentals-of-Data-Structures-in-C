75ff88ae1087b3048b0380240cbfa1c6c54d6c70
Martin Kersner <m.kersner@gmail.com>
2014-04-01 17:21:05 +0900

    added self referential structure problem including the right solution

---
+++ 2-3-4_Self-Referential_Structures.c
@@ -0,0 +1,42 @@
+/**
+ * FUNDAMENTALS OF DATA STUCTURES IN C 2ND EDITION
+ * (Horowitz, Sahni, Anderson-Freed)
+ *
+ * 2.3.4 Self-Referential Structures (63p)
+ *
+ * Gcc:
+ * gcc (Ubuntu/Linaro 4.8.1-10ubuntu9) 4.8.1
+ *
+ * Compile:
+ * gcc -ansi -std=c89 2-3-4_Self-Referential_Structures.c
+ *
+ * Date:
+ * 01/04/2014
+ *
+ * Output:
+ * 2-3-4_Self-Referential_Structures.c: In function ‘main’:
+ * 2-3-4_Self-Referential_Structures.c:31:5: error: unknown type name ‘list’
+ * 2-3-4_Self-Referential_Structures.c:32:3: warning: useless storage class specifier in empty declaration [enabled by default]
+ */
+
+#include <stdlib.h>
+
+int main (void)
+{
+  /* Self-Referential Structure according to book.
+   * CANNOT BE COMPILED!
+   */
+  typedef struct list {
+    char data;
+    list *link;
+  };
+
+  /* One of the ways how to define Self-Referential Structure.
+   */
+  typedef struct list2 {
+    char data;
+    struct list2* link;
+  } list2;
+
+  return EXIT_SUCCESS;
+}
